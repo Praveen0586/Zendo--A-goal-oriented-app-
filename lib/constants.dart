8e9a8f8789be5b2407da40b74bfb850df8da2a55
Praveen0586 <sureshpraveen0586@gmail.com>
2025-06-28 00:14:12 +0530

    Remove all files from gh-pages

--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-bool isDarkMode = false;
-ValueNotifier<bool> isDarkModeNotifier = ValueNotifier(false);
-var systemBrightness;
-
-// ValueNotifier<bool> isChekedList = ValueNotifier([]);
--- lib/hiv_/models.dart
+++
@@ -1,7 +0,0 @@
-class Todo {
-  int id;
-  String message;
-  bool isEnabled;
-
-  Todo({required this.id, required this.message, required this.isEnabled});
-}
